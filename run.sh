23269561b2fcff81649542707fa5aec3a57c1e57
Donovan McKelvey <Donovan.J.McKelvey@gmail.com>
2014-05-12 08:42:50 -0700

    option to hide stdout

--- run.sh
+++ run.sh
@@ -1,2 +1,12 @@
-java -classpath lib/antlr-3.3-complete.jar:bin/:. Mini "$@"
+#!/bin/bash
+
+loc=""
+vars="$@"
+if [ "$1" == "-no_out" ];
+then
+   loc=" > /dev/null"
+   vars="${*:2}"
+fi
+
+sh -c "java -classpath lib/antlr-3.3-complete.jar:bin/:. Mini $vars $loc"
 
