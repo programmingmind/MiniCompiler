4e564a1feca909654da94dd668e7acb8b7b343b4
Donovan McKelvey <Donovan.J.McKelvey@gmail.com>
2014-06-01 15:47:41 -0700

    benchmark test script and renaming

---
+++ testBenchmarks.sh
@@ -0,0 +1,17 @@
+find input/benchmarks -maxdepth 1 -mindepth 1 -type d \( ! -name . \) -exec bash -c \
+'
+echo "";
+echo $(basename {});
+./run.sh -no_out {}/$(basename {}).mini;
+gcc asm/$(basename {}).s;
+./$(basename {}) < {}/input > out;
+diff out {}/output > diffout;
+SIZE=$(du diffout | cut -f 1);
+if [  "$SIZE" -gt 0 ]; then
+   echo "FAILED";
+else
+   echo "Passed";
+fi;
+rm out;
+rm diffout;
+' \;
